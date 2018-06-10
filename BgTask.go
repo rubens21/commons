ae689ad5c5ee4f717b650c2a936b60edd1fe5b74
Rubens Silva <rubens21@gmail.com>
2018-06-10 11:08:32 -0400

    fixing bg task

--- BgTask.go
+++ BgTask.go
@@ -7,6 +7,7 @@ type Task struct {
 	task        func(*Task)
 	stopchan    chan struct{}
 	running 	bool
+	stopRequested 	bool
 }
 
 func NewTask(task func(*Task)) *Task {
@@ -21,7 +22,6 @@ func (t *Task) Start()  {
 	go func(){ // work in background
 		// TODO: do setup work
 		defer func(){
-			//bug! It does not work!
 			if t.OnStop != nil {
 				t.OnStop(t)
 			}
@@ -38,6 +38,11 @@ func (t *Task) Start()  {
 	}()
 }
 
-func (t *Task) Stop()  {
+func (t *Task) RequestStop()  {
+	t.stopRequested = true
 	close(t.stopchan)  // tell it to stop
+}
+
+func (t *Task) StopRequested() bool {
+	return t.stopRequested
 }
